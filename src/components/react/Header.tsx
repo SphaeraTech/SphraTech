fb70a9f42a909fda2beaf4b8dd99d988e8f64d81
cipher450 <algeriadelmoundo@djak.com>
2024-11-08 23:08:01 +0100

    ✨ add mobile friendly navbar

--- src/components/react/Header.tsx
+++ src/components/react/Header.tsx
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { HoverBorderGradient } from "./ui/HoverBorderGradient";
 import { useTranslations } from "../../i18n/utils";
 
 export default function NavBar({ lang = "en" }) {
   const t = useTranslations(null, lang);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <header
-      className="flex items-center bg-[#0F0F0F66]/10 justify-between absolute w-full z-20 backdrop-filter 
-backdrop-blur-md px-8 py-4
-"
+      className="flex items-center bg-[#0F0F0F66]/10 justify-between absolute w-full z-50 backdrop-filter 
+      backdrop-blur-md px-8 py-4"
     >
-      <div>
+      <div className="flex items-center">
         <a href="/" className="flex items-center gap-2">
-          <img src="/mainLogo.webp" alt="" width="64px" />
+          <img src="/mainLogo.webp" alt="SphæraTech Logo" width="64px" />
           <span className="text-white text-2xl font-bold">SphæraTech</span>
         </a>
       </div>
-      <nav>
-        <ul className="flex flex-row text-white items-center gap-5 justify-between font-medium">
+
+      <div className="flex md:hidden">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="text-white focus:outline-none"
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+      </div>
+
+      <nav
+        className={`${
+          isOpen ? "block" : "hidden"
+        } md:flex flex-col md:flex-row items-center text-white gap-5 font-medium absolute md:static top-full left-0 w-full bg-[#0F0F0F] md:bg-transparent md:w-auto md:mt-0  p-4 md:p-0 z-50`}
+      >
+        <ul className="flex flex-col md:flex-row gap-4 md:gap-5">
           <li className="hover:text-[#e14553] cursor-pointer">
             <a href={`/${lang}`}>{t("nav.home")}</a>
           </li>
@@ -34,7 +61,7 @@ backdrop-blur-md px-8 py-4
           <li>
             <a
               href="#"
-              className="border-2 border-white px-5 py-1 rounded-full bg-gray-600/30 "
+              className="border-2 border-white px-5 py-1 rounded-full bg-gray-600/30"
             >
               Contact
             </a>
